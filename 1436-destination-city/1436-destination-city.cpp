1d7acdb12a9da70588ab667281290d3a5089b339
srivarshni7 <srivarshnimuthu07@gmail.com>
2026-07-31 20:06:11 +0530

    Time: 2 ms (48.86%), Space: 15.1 MB (49.3%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+      unordered_map<string,int>mp;
+      for(int i=0;i<paths.size();i++)
+      {
+        mp[paths[i][0]]++;
+      }
+     for(int i=0;i<paths.size();i++)
+     {
+        if(mp[paths[i][1]]==0)
+        {
+            return paths[i][1];
+        }
+     }
+     return "";
+        
+    }
+};
